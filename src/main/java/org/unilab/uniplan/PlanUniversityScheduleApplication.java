979f7850a52c6699ab95a0275a80971fedc0cc3e
pencho-malinov <pencho.malinov@mycomplianceoffice.com>
2025-02-10 14:04:38 +0200

    init project

---
+++ src/main/java/org/unilab/uniplan/PlanUniversityScheduleApplication.java
@@ -0,0 +1,13 @@
+package org.unilab.uniplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanUniversityScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanUniversityScheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/unilab/uniplan/PlanUniversityScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.unilab.uniplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanUniversityScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

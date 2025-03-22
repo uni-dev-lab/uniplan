96c79f59d64866bfe9173cbb6d65482f7bb09512
gslavchev <galinslavchev044@gmail.com>
2025-03-22 07:17:10 +0200

    [Task UNI-12] create Room entity

---
+++ src/main/java/org/unilab/uniplan/room/Room.java
@@ -0,0 +1,29 @@
+package org.unilab.uniplan.room;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.unilab.uniplan.common.model.BaseEntity;
+import org.unilab.uniplan.faculty.Faculty;
+
+@Entity
+@Table(name = "ROOM")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Room extends BaseEntity {
+    
+    @ManyToOne
+    @JoinColumn(name = "FACULTY_ID", referencedColumnName = "ID", nullable = false)
+    private Faculty faculty;
+    @Column(name = "ROOM_NUMBER" , nullable = false)
+    private String roomNumber;
+
+}

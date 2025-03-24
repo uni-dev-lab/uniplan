ba5b97f8b3f9b9f090a94a2b826e6caa0415e857
georgi-g-likov <georgilikov2@gmail.com>
2025-03-24 20:20:35 +0200

    UNI-15 (#15)
    Added Student_Group entity and branch

---
+++ src/main/java/org/unilab/uniplan/studentgroup/StudentGroup.java
@@ -0,0 +1,31 @@
+package org.unilab.uniplan.studentgroup;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import org.unilab.uniplan.common.model.BaseEntity;
+import org.unilab.uniplan.coursegroup.CourseGroup;
+import org.unilab.uniplan.student.Student;
+import java.util.List;
+
+@Entity
+@Table(name = "STUDENT_GROUP")
+public class StudentGroup extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "STUDENT_ID", nullable = false)
+    private List<Student> studentList;
+
+    @ManyToOne
+    @JoinColumn(name = "GROUP_ID")
+    private CourseGroup courseGroup;
+
+    @Column(name = "GROUP_NAME", nullable = false)
+    private String groupName;
+
+    @Column(name = "MAX_GROUP", nullable = false)
+    private int maxGroup;
+}

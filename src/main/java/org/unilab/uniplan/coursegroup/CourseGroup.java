2a2fd0da3f2ad8ac0590474c9e4735434e98770c
georgi-g-likov <georgilikov2@gmail.com>
2025-03-27 20:55:27 +0200

    UNI-26 (#19)
    * UNI-26
    
    Migrated fields from StudentGroup to CourseGroup
    
    * UNI-25 Optimize fields lengths across the database tables (#17)
    
    Optimized all field lengths across the database tables
    
    * UNI-26
    
    fixed merge conflict
    
    * UNI-26
    
    Changed fixed fields
    
    ---------
    
    Co-authored-by: Nikolay Chaushev <nikichaushev2004@gmail.com>

--- src/main/java/org/unilab/uniplan/coursegroup/CourseGroup.java
+++ src/main/java/org/unilab/uniplan/coursegroup/CourseGroup.java
@@ -1,5 +1,6 @@
 package org.unilab.uniplan.coursegroup;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -22,4 +23,10 @@ public class CourseGroup extends BaseEntity {
     @ManyToOne
     @JoinColumn(name = "COURSE_ID", nullable = false)
     private Course course;
+    
+    @Column(name = "GROUP_NAME", nullable = false)
+    private String groupName;
+
+    @Column(name = "MAX_GROUP", nullable = false)
+    private int maxGroup;
 }

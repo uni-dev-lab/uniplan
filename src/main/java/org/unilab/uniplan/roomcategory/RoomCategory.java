f77477d96be4af516a30fcb6f2d94389fdbf9341
gslavchev <galinslavchev044@gmail.com>
2025-04-01 11:15:25 +0300

    [Task UNI_30] add embeddedId for the RoomCategory entity

--- src/main/java/org/unilab/uniplan/roomcategory/RoomCategory.java
+++ src/main/java/org/unilab/uniplan/roomcategory/RoomCategory.java
@@ -1,32 +1,38 @@
 package org.unilab.uniplan.roomcategory;
 
+import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
 import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.unilab.uniplan.category.Category;
-import org.unilab.uniplan.common.model.BaseEntity;
+import org.unilab.uniplan.common.model.AuditableEntity;
 import org.unilab.uniplan.room.Room;
 
 @Entity
-@Table(name = "ROOM_CATEGORY",  
-    uniqueConstraints=
-    @UniqueConstraint(columnNames={"ROOM_ID", "CATEGORY_ID"}))
+@Table(name = "ROOM_CATEGORY")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class RoomCategory extends BaseEntity{
-
-    @ManyToOne
+public class RoomCategory extends AuditableEntity {
+    
+    @EmbeddedId
+    private RoomCategoryId id;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("roomId")
     @JoinColumn(name = "ROOM_ID", referencedColumnName = "ID", nullable = false)
     private Room room;
-    @ManyToOne
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("categoryId")
     @JoinColumn(name = "CATEGORY_ID", referencedColumnName = "ID", nullable = false)
     private Category category;
 }

301c3459f84d0f43ce1966e4e48260e8810ca2aa
nikich55 <nikichaushev2004@gmail.com>
2025-04-11 12:50:20 +0300

    Enabled JPA Auditing
    Added @EnableJPAAuditing anotation, enabeling JPA Auditing.

--- src/main/java/org/unilab/uniplan/PlanUniversityScheduleApplication.java
+++ src/main/java/org/unilab/uniplan/PlanUniversityScheduleApplication.java
@@ -2,12 +2,14 @@ package org.unilab.uniplan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PlanUniversityScheduleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PlanUniversityScheduleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PlanUniversityScheduleApplication.class, args);
+    }
 
 }

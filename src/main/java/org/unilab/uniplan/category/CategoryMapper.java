766c3a7787da0aa56f93b4f6b7ded714619e9d8b
Nikolay Chaushev <nikichaushev2004@gmail.com>
2025-04-21 10:37:06 +0300

    UNI-39 (#34)
    * UNI-39
    
    Updated Category Api
    
    * Updated Category Api
    
    Updated the Category Api with the needed changes.
    
    * Added missed final to fields

--- src/main/java/org/unilab/uniplan/category/CategoryMapper.java
+++ src/main/java/org/unilab/uniplan/category/CategoryMapper.java
@@ -1,11 +1,30 @@
 package org.unilab.uniplan.category;
 
+import java.util.List;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.unilab.uniplan.category.dto.CategoryDto;
+import org.unilab.uniplan.category.dto.CategoryRequestDto;
+import org.unilab.uniplan.category.dto.CategoryResponseDto;
 
-@Mapper(componentModel = "spring")
+@Mapper
 public interface CategoryMapper {
 
-    CategoryDto toDto(Category category);
+    Category toEntity(final CategoryDto categoryDto);
 
-    Category toEntity(CategoryDto categoryDto);
+    CategoryDto toDto(final Category category);
+
+    CategoryDto toInternalDto(final CategoryRequestDto categoryRequestDto);
+
+    CategoryResponseDto toResponseDto(final CategoryDto categoryDto);
+
+    List<CategoryDto> toDtoList(final List<Category> categories);
+
+    List<CategoryResponseDto> toResponseDtoList(final List<CategoryDto> categories);
+
+    @Mapping(target = "roomType", source = "categoryDto.roomType")
+    @Mapping(target = "capacity", source = "categoryDto.capacity")
+    void updateEntityFromDto(final CategoryDto categoryDto,
+                             @MappingTarget final Category category);
 }

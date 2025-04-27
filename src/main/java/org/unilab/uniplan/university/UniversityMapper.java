89e963c1663625e7dbfcc0f6173d5730331aed81
Nikolay Chaushev <nikichaushev2004@gmail.com>
2025-04-27 23:02:37 +0300

    UNI-35 University API (#38)
    * UNI-39
    
    Updated Category Api
    
    * Updated Category Api
    
    Updated the Category Api with the needed changes.
    
    * Added missed final to fields
    
    * UNI-52 Created JpaAudtingConfig
    
    Created JpaAuditingConfig class.
    
    * UNI - 35 University Api
    
    Created CRUD opertations for university entity, added @Setter to the Base Entity.
    
    * Removed unnecessary mapping

--- src/main/java/org/unilab/uniplan/university/UniversityMapper.java
+++ src/main/java/org/unilab/uniplan/university/UniversityMapper.java
@@ -1,12 +1,29 @@
 package org.unilab.uniplan.university;
 
+import java.util.List;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.unilab.uniplan.university.dto.UniversityDto;
+import org.unilab.uniplan.university.dto.UniversityRequestDto;
+import org.unilab.uniplan.university.dto.UniversityResponseDto;
 
-@Mapper(componentModel = "spring")
+@Mapper
 public interface UniversityMapper {
 
-    UniversityDto toDto(University university);
+    University toEntity(final UniversityDto universityDto);
 
-    University toEntity(UniversityDto universityDto);
+    UniversityDto toDto(final University university);
+
+    UniversityDto toInternalDto(final UniversityRequestDto universityRequestDto);
+
+    UniversityResponseDto toResponseDto(final UniversityDto universityDto);
+
+    List<UniversityDto> toDtoList(final List<University> universities);
+
+    List<UniversityResponseDto> toResponseDtoList(final List<UniversityDto> universities);
+
+    @Mapping(target = "id", ignore = true)
+    void updateEntityFromDto(final UniversityDto universityDto,
+                             @MappingTarget final University university);
 }
-

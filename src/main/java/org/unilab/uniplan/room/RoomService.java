f605c44150446e581783038975865312f98cf5f2
nikich55 <nikichaushev2004@gmail.com>
2025-04-28 17:06:51 +0300

    UNI - 38 Room Api
    Created CRUD operations for Room entity.

--- src/main/java/org/unilab/uniplan/room/RoomService.java
+++ src/main/java/org/unilab/uniplan/room/RoomService.java
@@ -1,74 +1,54 @@
 package org.unilab.uniplan.room;
 
+import java.text.MessageFormat;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.unilab.uniplan.faculty.Faculty;
-import org.unilab.uniplan.faculty.FacultyService;
+import org.unilab.uniplan.room.dto.RoomDto;
 
 @Service
+@RequiredArgsConstructor
 public class RoomService {
 
+    public static final String ROOM_NOT_FOUND = "Room with ID {0} not found.";
+
     private final RoomRepository roomRepository;
-    private final FacultyService facultyService;
     private final RoomMapper roomMapper;
 
-    @Autowired
-    public RoomService(RoomRepository roomRepository,
-                       FacultyService facultyService,
-                       RoomMapper roomMapper) {
-        this.roomRepository = roomRepository;
-        this.facultyService = facultyService;
-        this.roomMapper = roomMapper;
-    }
-
     @Transactional
-    public RoomDto createRoom(RoomDto roomDto) {
-        Faculty faculty = facultyService.getFaculty(roomDto.facultyId())
-                                        .orElseThrow(() -> new IllegalArgumentException(
-                                            "Faculty not found"));
-        Room room = roomMapper.toEntity(roomDto);
-        room.setRoomNumber(roomDto.roomNumber());
-        room.setFaculty(faculty);
-        room = roomRepository.save(room);
-        return roomMapper.toDto(room);
+    public RoomDto createRoom(final RoomDto roomDto) {
+        final Room room = roomMapper.toEntity(roomDto);
+
+        return roomMapper.toDto(roomRepository.save(room));
     }
 
     public List<RoomDto> getAllRooms() {
-        List<Room> rooms = roomRepository.findAll();
-        return rooms.stream().map(roomMapper::toDto).toList();
+        return roomMapper.toDtoList(roomRepository.findAll());
     }
 
-    public Optional<RoomDto> getRoomById(UUID id) {
-        Optional<Room> room = roomRepository.findById(id);
-        return room.map(roomMapper::toDto);
-    }
-
-    @Transactional
-    public Optional<RoomDto> updateRoom(UUID id, RoomDto roomDto) {
-        Optional<Room> existingRoom = roomRepository.findById(id);
-        if (existingRoom.isPresent()) {
-            Faculty faculty = facultyService
-                .getFaculty(roomDto.facultyId())
-                .orElseThrow(() -> new IllegalArgumentException("Faculty not found"));
-            Room room = existingRoom.get();
-            room.setRoomNumber(roomDto.roomNumber());
-            room.setFaculty(faculty);
-            room = roomRepository.save(room);
-            return Optional.of(roomMapper.toDto(room));
-        }
-        return Optional.empty();
+    public Optional<RoomDto> getRoomById(final UUID id) {
+        return roomRepository.findById(id)
+                             .map(roomMapper::toDto);
     }
 
     @Transactional
-    public boolean deleteRoom(UUID id) {
-        if (roomRepository.existsById(id)) {
-            roomRepository.deleteById(id);
-            return true;
-        }
-        return false;
+    public Optional<RoomDto> updateRoom(final UUID id, final RoomDto roomDto) {
+        return roomRepository.findById(id)
+                             .map(existingRoom -> {
+                                 roomMapper.updateEntityFromDto(roomDto, existingRoom);
+
+                                 return roomMapper.toDto(roomRepository.save(existingRoom));
+                             });
+    }
+
+    @Transactional
+    public void deleteRoom(final UUID id) {
+        final Room room = roomRepository.findById(id)
+                                        .orElseThrow(() -> new RuntimeException(
+                                            MessageFormat.format(ROOM_NOT_FOUND, id)));
+        roomRepository.delete(room);
     }
 }

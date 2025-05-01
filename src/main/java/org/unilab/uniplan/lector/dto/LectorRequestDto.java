5072696119e8822c988221bcdf464ec306b127cb
georgi-g-likov <georgilikov2@gmail.com>
2025-05-01 22:53:24 +0300

    Uni 47 (#29)
    * Embedded id for Student group
    
    * Update StudentGroup.java
    
    bug fixes
    
    * UNI-47
    
    Created Service and Controller for Lector
    
    * UNI-47
    
    fixed issues with error handling and removed unused Repos
    
    * UNI-47
    
    Added Lector DTO and mapper and fixed some issues in the service and controller
    
    * UNI-47
    
    Update controller and service
    
    * UNI-47
    
    Replaced manual cast and logic with MapStruct
    
    * UNI-47
    
    Removed unused imports and changed error message
    
    * UNI-47
    
    fixed updateLector to work with dto
    
    * UNI-47
    
    Refractored LectorDto to Request and Response dtos and updated controller and service classes

---
+++ src/main/java/org/unilab/uniplan/lector/dto/LectorRequestDto.java
@@ -0,0 +1,27 @@
+package org.unilab.uniplan.lector.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.util.UUID;
+
+public record LectorRequestDto(
+
+    @NotNull(message = "Faculty ID cannot be null")
+    UUID facultyId,
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Please provide a valid email address")
+    String email,
+
+    @NotBlank(message = "First name is required")
+    @Size(max = 100)
+    String firstName,
+
+    @NotBlank(message = "Last name is required")
+    @Size(max = 100)
+    String lastName
+) {
+
+}

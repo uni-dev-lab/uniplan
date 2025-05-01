322d75632df247f3664d3a6a25b8ea9bb13fb75f
Nikolay Chaushev <nikichaushev2004@gmail.com>
2025-05-01 09:52:21 +0300

    UNI-37 Department Api (#40)
    * UNI-37 Department Api
    
    Created CRUD API for Department entity>
    
    * Changed constants access modifier

--- src/main/java/org/unilab/uniplan/department/DepartmentService.java
+++ src/main/java/org/unilab/uniplan/department/DepartmentService.java
@@ -1,82 +1,59 @@
 package org.unilab.uniplan.department;
 
+import java.text.MessageFormat;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.unilab.uniplan.faculty.Faculty;
-import org.unilab.uniplan.faculty.FacultyService;
+import org.unilab.uniplan.department.dto.DepartmentDto;
 
 @Service
+@RequiredArgsConstructor
 public class DepartmentService {
 
+    private static final String DEPARTMENT_NOT_FOUND = "Department with ID {0} not found.";
+
     private final DepartmentRepository departmentRepository;
-    private final FacultyService facultyService;
     private final DepartmentMapper departmentMapper;
 
-    @Autowired
-    public DepartmentService(DepartmentRepository departmentRepository,
-                             FacultyService facultyService,
-                             DepartmentMapper departmentMapper) {
-        this.departmentRepository = departmentRepository;
-        this.facultyService = facultyService;
-        this.departmentMapper = departmentMapper;
-    }
-
     @Transactional
-    public DepartmentDto createDepartment(DepartmentDto departmentDto) {
-        Faculty faculty = facultyService.getFaculty(departmentDto.facultyId())
-                                        .orElseThrow(() -> new IllegalArgumentException(
-                                            "Faculty not found"));
+    public DepartmentDto createDepartment(final DepartmentDto departmentDto) {
+        final Department department = departmentMapper.toEntity(departmentDto);
 
-        Department department = departmentMapper.toEntity(departmentDto);
-        department.setDepartmentName(departmentDto.departmentName());
-        department.setFaculty(faculty);
-        department = departmentRepository.save(department);
-
-        return departmentMapper.toDto(department);
+        return departmentMapper.toDto(departmentRepository.save(department));
     }
 
     public List<DepartmentDto> getAllDepartments() {
-        List<Department> departments = departmentRepository.findAll();
-        return departments.stream()
-                          .map(departmentMapper::toDto)
-                          .toList();
+        final List<Department> departments = departmentRepository.findAll();
+
+        return departmentMapper.toDtoList(departments);
     }
 
-    public Optional<DepartmentDto> getDepartmentById(UUID id) {
-        Optional<Department> department = departmentRepository.findById(id);
-        return department.map(departmentMapper::toDto);
-    }
-
-    @Transactional
-    public Optional<DepartmentDto> updateDepartment(UUID id, DepartmentDto departmentDto) {
-        Optional<Department> existingDepartment = departmentRepository.findById(id);
-
-        if (existingDepartment.isPresent()) {
-            Faculty faculty = facultyService.getFaculty(departmentDto.facultyId())
-                                            .orElseThrow(() -> new IllegalArgumentException(
-                                                "Faculty not found"));
-
-            Department department = existingDepartment.get();
-            department.setDepartmentName(departmentDto.departmentName());
-            department.setFaculty(faculty);
-
-            department = departmentRepository.save(department);
-            return Optional.of(departmentMapper.toDto(department));
-        }
-
-        return Optional.empty();
+    public Optional<DepartmentDto> getDepartmentById(final UUID id) {
+        return departmentRepository.findById(id)
+                                   .map(departmentMapper::toDto);
     }
 
     @Transactional
-    public boolean deleteDepartment(UUID id) {
-        if (departmentRepository.existsById(id)) {
-            departmentRepository.deleteById(id);
-            return true;
-        }
-        return false;
+    public Optional<DepartmentDto> updateDepartment(final UUID id,
+                                                    final DepartmentDto departmentDto) {
+        return departmentRepository.findById(id)
+                                   .map(existingDepartment -> {
+                                       departmentMapper.updateEntityFromDto(departmentDto,
+                                                                            existingDepartment);
+                                       return departmentMapper.toDto(departmentRepository.save(
+                                           existingDepartment));
+                                   });
+    }
+
+    @Transactional
+    public void deleteDepartment(final UUID id) {
+        final Department department = departmentRepository.findById(id)
+                                                          .orElseThrow(() -> new RuntimeException(
+                                                              MessageFormat.format(
+                                                                  DEPARTMENT_NOT_FOUND, id)));
+        departmentRepository.delete(department);
     }
 }

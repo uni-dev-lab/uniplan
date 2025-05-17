016bf5870896317da3b65bd1ce5b7aa1ce0c4c1e
georgi-g-likov <georgilikov2@gmail.com>
2025-05-17 10:35:27 +0300

    UNI-49 (#36)
    * UNI-49
    
    Created program dtos, controller and service
    
    * UNI-49
    
    Changed dtos to contain UUID instead of other dtos, changed mapping
    
    * UNI-49
    
    Changed error string to private

---
+++ src/main/java/org/unilab/uniplan/program/ProgramMapper.java
@@ -0,0 +1,33 @@
+package org.unilab.uniplan.program;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.unilab.uniplan.program.dto.ProgramDto;
+import org.unilab.uniplan.program.dto.ProgramRequestDto;
+import org.unilab.uniplan.program.dto.ProgramResponseDto;
+import java.util.List;
+
+@Mapper
+public interface ProgramMapper {
+
+    @Mapping(source = "courseId", target = "course.id")
+    @Mapping(source = "programDisciplinesIds", target = "programDisciplines")
+    Program toEntity(final ProgramDto programDto);
+
+    @Mapping(source = "course.id", target = "courseId")
+    @Mapping(source = "programDisciplines", target = "programDisciplinesIds")
+    ProgramDto toDto(final Program program);
+    @Mapping(target = "id", ignore = true)
+    @Mapping(source = "programDisciplinesId", target = "programDisciplinesIds")
+    ProgramDto toInternalDto(final ProgramRequestDto requestDto);
+
+    List<ProgramDto> toDtoList(final List<Program> programs);
+
+    List<ProgramResponseDto> toResponseDtoList(final List<ProgramDto> programDtos);
+
+    @Mapping(target = "course.id", source = "programDto.courseId")
+    @Mapping(target = "programDisciplines", source = "programDto.programDisciplinesIds")
+    void updateEntityFromDto(final ProgramDto programDto,
+                             @MappingTarget final Program program);
+}

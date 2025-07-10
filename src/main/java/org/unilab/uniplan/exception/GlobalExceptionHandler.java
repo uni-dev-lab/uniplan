18bf92351e1b0facd8bcc2f538950757b1b854b4
Nikolay Chaushev <nikichaushev2004@gmail.com>
2025-07-10 14:58:03 +0300

    Update global exception handler

--- src/main/java/org/unilab/uniplan/exception/GlobalExceptionHandler.java
+++ src/main/java/org/unilab/uniplan/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package org.unilab.uniplan.exception;
 import jakarta.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
 import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,17 +12,19 @@ import org.springframework.web.method.annotation.HandlerMethodValidationExceptio
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.server.ResponseStatusException;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-
     //Handles validation exceptions triggered by method argument validation failures (e.g. @Valid)
     @ExceptionHandler(HandlerMethodValidationException.class)
-    public ResponseEntity<ErrorResponse> handleValidationException(HandlerMethodValidationException ex,
-                                                                   HttpServletRequest request) {
+    public ResponseEntity<ErrorResponse> handleValidationException(final HandlerMethodValidationException ex,
+                                                                   final HttpServletRequest request) {
+        log.info(ex.getReason());
+
         return new ResponseEntity<>(
             new ErrorResponse(
-                "Validation failed: invalid request data",
+                "Невалидни данни!",
                 HttpStatus.BAD_REQUEST.value(),
                 LocalDateTime.now(),
                 request.getRequestURI()
@@ -32,11 +35,13 @@ public class GlobalExceptionHandler {
 
     // Handles exceptions thrown with specific HTTP status (like 404, 400) via ResponseStatusException
     @ExceptionHandler(ResponseStatusException.class)
-    public ResponseEntity<ErrorResponse> handleResponseStatusException(ResponseStatusException ex,
-                                                                       HttpServletRequest request) {
+    public ResponseEntity<ErrorResponse> handleResponseStatusException(final ResponseStatusException ex,
+                                                                       final HttpServletRequest request) {
+        log.info(ex.getReason());
+
         return new ResponseEntity<>(
             new ErrorResponse(
-                ex.getReason(),
+                "Липса на данни!",
                 ex.getStatusCode().value(),
                 LocalDateTime.now(),
                 request.getRequestURI()
@@ -47,20 +52,22 @@ public class GlobalExceptionHandler {
 
     // Handles type mismatch exceptions (e.g., when a path variable cannot be converted to the expected type)
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    public ResponseEntity<ErrorResponse> handleTypeMismatch(MethodArgumentTypeMismatchException ex,
-                                                            HttpServletRequest request) {
+    public ResponseEntity<ErrorResponse> handleTypeMismatch(final MethodArgumentTypeMismatchException ex,
+                                                            final HttpServletRequest request) {
 
-        String expectedType = Optional.ofNullable(ex.getRequiredType())
+        final String expectedType = Optional.ofNullable(ex.getRequiredType())
                                       .map(Class::getSimpleName)
                                       .orElse("unknown type");
 
-        String message = "Invalid parameter: "
+        final String message = "Invalid parameter: "
                          + ex.getName()
                          + " should be of type "
                          + expectedType;
 
+        log.info(message);
+
         return new ResponseEntity<>(new ErrorResponse(
-            message,
+            "Грешка!",
             HttpStatus.BAD_REQUEST.value(),
             LocalDateTime.now(),
             request.getRequestURI()
@@ -69,11 +76,13 @@ public class GlobalExceptionHandler {
 
     // Handles generic runtime exceptions that are not caught by more specific handlers
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException ex,
-                                                                HttpServletRequest request) {
+    public ResponseEntity<ErrorResponse> handleRuntimeException(final RuntimeException ex,
+                                                                final HttpServletRequest request) {
+
+        log.info(ex.getMessage());
 
         return new ResponseEntity<>(new ErrorResponse(
-            ex.getMessage(),
+            "Грешка в сървъра!",
             HttpStatus.INTERNAL_SERVER_ERROR.value(),
             LocalDateTime.now(),
             request.getRequestURI()
@@ -82,9 +91,11 @@ public class GlobalExceptionHandler {
 
     // Handles any uncaught exceptions, logs the exception type, and returns HTTP 500 with error message
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleAnyException(Exception ex) {
+    public ResponseEntity<String> handleAnyException(final Exception ex) {
+
+        log.info(ex.getMessage());
 
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                             .body("Error: " + ex.getMessage());
+                             .body("Грешка!");
     }
 }

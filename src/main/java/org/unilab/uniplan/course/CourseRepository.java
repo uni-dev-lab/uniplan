f16d857d3130479ccc7feb862d9c81e29e547c06
Martin Aleksandrov <121686622+MartinAleksandrov@users.noreply.github.com>
2025-09-04 15:07:43 +0300

    UNI-140 Created API Endpoint for Student (#74)
    * Added new endpoint.
    
    This endpoint returns all major courses by major id.
    
    * New endpoint
    
    This endpoint returns all faculty majors by faculty id.
    
    * Returns list of CourseDtos.
    
    * Find all courses by their Major id.
    
    * Returns all majors by their faculty id.
    
    * Added Unit test for new endpoints.
    
    These tests cover two scenarios: one checks whether the correct object is returned, and the other verifies that an empty list is returned.
    
    * Unit tests for new endpoints.
    
    * Returns list of majors.
    
    * Changing name convention to kebab-case.
    
    * Verifying invocation of method.
    
    * Changed endpoint naming.
    
    * Removed empty spaces.

--- src/main/java/org/unilab/uniplan/course/CourseRepository.java
+++ src/main/java/org/unilab/uniplan/course/CourseRepository.java
@@ -1,5 +1,6 @@
 package org.unilab.uniplan.course;
 
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,4 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CourseRepository extends JpaRepository<Course, UUID> {
 
+    public List<Course> findAllByMajorId(UUID majorId);
 }

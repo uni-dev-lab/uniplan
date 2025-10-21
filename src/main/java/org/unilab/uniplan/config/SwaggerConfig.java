b6cdac5bb373a656cd6b8531ade4aa998b7a99a6
Sideroff-D <dimitar.k.siderov@gmail.com>
2025-10-21 15:38:01 +0300

    -Use status code constants instead of strings in SwaggerConfig

--- src/main/java/org/unilab/uniplan/config/SwaggerConfig.java
+++ src/main/java/org/unilab/uniplan/config/SwaggerConfig.java
@@ -6,6 +6,7 @@ import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.responses.ApiResponse;
+import org.springframework.http.HttpStatus;
 
 @Configuration
 @OpenAPIDefinition(info = @Info(title = "UniPlan - API", version = "1.0",
@@ -20,19 +21,19 @@ public class SwaggerConfig {
                   var responses = operation.getResponses();
 
                   // for all
-                  responses.addApiResponse("400", new ApiResponse().description("Invalid input / Bad request"));
-                  responses.addApiResponse("500", new ApiResponse().description("Internal server error"));
+                  responses.addApiResponse(String.valueOf(HttpStatus.BAD_REQUEST.value()), new ApiResponse().description(HttpStatus.BAD_REQUEST.getReasonPhrase()));
+                  responses.addApiResponse(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()), new ApiResponse().description(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase()));
 
                   // custom for each method
                   switch (httpMethod) {
-                      case POST -> responses.addApiResponse("201", new ApiResponse().description("Resource created"));
+                      case POST -> responses.addApiResponse(String.valueOf(HttpStatus.CREATED.value()), new ApiResponse().description(HttpStatus.CREATED.getReasonPhrase()));
                       case PUT -> {
-                          responses.addApiResponse("200", new ApiResponse().description("Resource updated"));
-                          responses.addApiResponse("404", new ApiResponse().description("Resource not found"));
+                          responses.addApiResponse(String.valueOf(HttpStatus.OK.value()), new ApiResponse().description(HttpStatus.OK.getReasonPhrase()));
+                          responses.addApiResponse(String.valueOf(HttpStatus.NOT_FOUND.value()), new ApiResponse().description(HttpStatus.NOT_FOUND.getReasonPhrase()));
                       }
                       case DELETE -> {
-                          responses.addApiResponse("204", new ApiResponse().description("Resource deleted"));
-                          responses.addApiResponse("404", new ApiResponse().description("Resource not found"));
+                          responses.addApiResponse(String.valueOf(HttpStatus.NO_CONTENT.value()), new ApiResponse().description(HttpStatus.NO_CONTENT.getReasonPhrase()));
+                          responses.addApiResponse(String.valueOf(HttpStatus.NOT_FOUND.value()), new ApiResponse().description(HttpStatus.NOT_FOUND.getReasonPhrase()));
                       }
                   }
               })

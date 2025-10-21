40ecf57be6fc0ae15607ff56019474a515350c10
Ivaylo Iliev <iliev_evn@hotmail.com>
2025-10-21 13:36:22 +0300

    Update mapper to map major's courses

--- src/main/java/org/unilab/uniplan/major/MajorMapper.java
+++ src/main/java/org/unilab/uniplan/major/MajorMapper.java
@@ -4,11 +4,14 @@ import java.util.List;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.unilab.uniplan.course.CourseMapper;
+import org.unilab.uniplan.major.dto.MajorCoursesDto;
+import org.unilab.uniplan.major.dto.MajorCoursesResponseDto;
 import org.unilab.uniplan.major.dto.MajorDto;
 import org.unilab.uniplan.major.dto.MajorRequestDto;
 import org.unilab.uniplan.major.dto.MajorResponseDto;
 
-@Mapper
+@Mapper(uses = {CourseMapper.class})
 public interface MajorMapper {
 
     @Mapping(source = "facultyId", target = "faculty.id")
@@ -16,6 +19,9 @@ public interface MajorMapper {
 
     @Mapping(source = "faculty.id", target = "facultyId")
     MajorDto toDto(Major major);
+
+    @Mapping(source = "faculty.id", target = "facultyId")
+    MajorCoursesDto toFullDto(Major major);
 
     @Mapping(source = "facultyId", target = "faculty.id")
     void updateEntityFromDto(MajorDto majorDto, @MappingTarget Major major);
@@ -27,4 +33,9 @@ public interface MajorMapper {
     MajorResponseDto toResponseDto(MajorDto innerDto);
 
     List<MajorResponseDto> toResponseDtoList(List<MajorDto> majors);
+
+    @Mapping(source = "facultyId", target = "facultyId")
+    MajorCoursesResponseDto toFullResponseDto(MajorCoursesDto innerDto);
+
+    List<MajorCoursesResponseDto> toFullResponseDtoList(List<MajorCoursesDto> majors);
 }

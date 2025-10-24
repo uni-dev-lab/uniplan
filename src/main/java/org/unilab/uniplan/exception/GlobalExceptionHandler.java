dedd73ab961858ecb72e54ed0c938db8b742eeeb
Dimitar Siderov <dimitar.k.siderov@gmail.com>
2025-10-24 13:52:30 +0300

    Uni 135 swagger UI (#78)
    * -add swagger dependency and @Tag annotations for controllers
    
    * -add SwaggerConfig with global ApiResponse definitions (400, 500, POST/PUT/DELETE defaults)
    
    * -change comments to EN and improve code formatting for GitHub readability
    
    * -Use status code constants instead of strings in SwaggerConfig

--- src/main/java/org/unilab/uniplan/exception/GlobalExceptionHandler.java
+++ src/main/java/org/unilab/uniplan/exception/GlobalExceptionHandler.java
@@ -10,6 +10,7 @@ import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.HandlerMethodValidationException;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;

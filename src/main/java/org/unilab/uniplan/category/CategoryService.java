0847ed4528a52ba25ace9ce1c8f0a720fed374ce
Djesika Vacheva <stu2401261031@uni-plovdiv.bg>
2026-06-24 10:25:21 +0300

    Refactor CategoryService for facade pattern

--- src/main/java/org/unilab/uniplan/category/CategoryService.java
+++ src/main/java/org/unilab/uniplan/category/CategoryService.java
@@ -1,71 +1,30 @@
 package org.unilab.uniplan.category;
 
-import static org.unilab.uniplan.utils.ErrorConstants.CATEGORY_NOT_FOUND;
-
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import org.unilab.uniplan.category.dto.CategoryDto;
-import org.unilab.uniplan.exception.ResourceNotFoundException;
 
 @Service
 @RequiredArgsConstructor
 public class CategoryService {
 
     private final CategoryRepository categoryRepository;
-    private final CategoryMapper categoryMapper;
 
-    @Transactional
-    public CategoryDto createCategory(final CategoryDto categoryDto) {
-        final Category category = categoryMapper.toEntity(categoryDto);
-
-        return saveEntityAndConvertToDto(category);
+    public Category save(final Category category) {
+        return categoryRepository.save(category);
     }
 
-    public List<CategoryDto> getAllCategories() {
-        final List<Category> categories = categoryRepository.findAll();
-
-        return categoryMapper.toDtoList(categories);
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
     }
 
-    public CategoryDto getCategoryById(final UUID id) {
-        return categoryRepository.findById(id)
-                                 .map(categoryMapper::toDto)
-                                 .orElseThrow(() -> new ResourceNotFoundException(CATEGORY_NOT_FOUND.getMessage(
-                                     String.valueOf(id))));
+    public Optional<Category> getCategoryById(final UUID id) {
+        return categoryRepository.findById(id);
     }
 
-    @Transactional
-    public CategoryDto updateCategory(final UUID id, final CategoryDto categoryDto) {
-        return categoryRepository.findById(id)
-                                 .map(existingCategory -> updateEntityAndConvertToDto(
-                                     categoryDto,
-                                     existingCategory))
-                                 .orElseThrow(() -> new ResourceNotFoundException(CATEGORY_NOT_FOUND.getMessage(
-                                     String.valueOf(id))));
-    }
-
-
-    @Transactional
-    public void deleteCategory(final UUID id) {
-        final Category category = categoryRepository.findById(id)
-                                                    .orElseThrow(() -> new ResourceNotFoundException(
-                                                        CATEGORY_NOT_FOUND.getMessage(String.valueOf(id))));
+    public void deleteCategory(Category category) {
         categoryRepository.delete(category);
-    }
-
-
-    private CategoryDto updateEntityAndConvertToDto(final CategoryDto dto,
-                                                    final Category entity) {
-        categoryMapper.updateEntityFromDto(dto, entity);
-        return saveEntityAndConvertToDto(entity);
-    }
-
-    private CategoryDto saveEntityAndConvertToDto(final Category entity) {
-        final Category savedEntity = categoryRepository.save(entity);
-        return categoryMapper.toDto(savedEntity);
     }
 }

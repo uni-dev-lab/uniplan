b182de4fcd266de6890011e832d511d05a25fff7
Ivcho <stu2401261003@uni-plovdiv.bg>
2026-06-30 15:32:38 +0300

    fixed broken edit faculty on lector, added mapper tests

--- src/main/java/org/unilab/uniplan/lector/LectorMapper.java
+++ src/main/java/org/unilab/uniplan/lector/LectorMapper.java
@@ -1,9 +1,11 @@
 package org.unilab.uniplan.lector;
 
 import java.util.List;
+import java.util.UUID;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.unilab.uniplan.faculty.Faculty;
 import org.unilab.uniplan.lector.dto.LectorDto;
 import org.unilab.uniplan.lector.dto.LectorRequestDto;
 import org.unilab.uniplan.lector.dto.LectorResponseDto;
@@ -19,12 +21,15 @@ public interface LectorMapper {
     LectorDto toDto(Lector lector);
 
     @Mapping(target = "id", ignore = true)
-    @Mapping(source = "facultyId", target = "faculty.id")
+    @Mapping(source = "facultyId", target = "faculty")
     void updateEntity(LectorRequestDto requestDto, @MappingTarget Lector lector);
 
     @Mapping(source = "faculty.id", target = "facultyId")
     LectorResponseDto toResponseDto(Lector lector);
 
     List<LectorResponseDto> toResponseDtoList(List<Lector> lectors);
+
+    @Mapping(target = "id", source = "facultyId")
+    Faculty toFaculty(UUID facultyId);
 
 }

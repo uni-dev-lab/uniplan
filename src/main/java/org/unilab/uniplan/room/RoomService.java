81bd802beaa6cdd85a693a4fb64acef2daf80742
PIPetkova19 <PIPetkova19@codingburgas.bg>
2026-07-07 11:48:03 +0300

    Update roomService by implementing baseService

--- src/main/java/org/unilab/uniplan/room/RoomService.java
+++ src/main/java/org/unilab/uniplan/room/RoomService.java
@@ -3,62 +3,38 @@ package org.unilab.uniplan.room;
 import static org.unilab.uniplan.utils.ErrorConstants.ROOM_NOT_FOUND;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.unilab.uniplan.common.model.BaseService;
 import org.unilab.uniplan.exception.ResourceNotFoundException;
+import org.unilab.uniplan.room.dto.RoomRequestDto;
 
 @Service
 @RequiredArgsConstructor
-public class RoomService {
+public class RoomService implements BaseService<Room> {
 
     private final RoomRepository roomRepository;
-    private final RoomMapper roomMapper;
 
-    @Transactional
-    public RoomDto createRoom(final RoomDto roomDto) {
-        final Room room = roomMapper.toEntity(roomDto);
-
-        return saveEntityAndConvertToDto(room);
+    @Override
+    public void save(final Room room) {
+        roomRepository.save(room);
     }
 
-    public List<RoomDto> getAllRooms() {
-        return roomMapper.toDtoList(roomRepository.findAll());
+    @Override
+    public List<Room> getAll() {
+        return roomRepository.findAll();
     }
 
-    public RoomDto getRoomById(final UUID id) {
-        return roomRepository.findById(id)
-                             .map(roomMapper::toDto)
-                             .orElseThrow(() -> new ResourceNotFoundException(ROOM_NOT_FOUND.getMessage(
-                                 String.valueOf(id))));
+    @Override
+    public Optional<Room> getById(final UUID id) {
+        return roomRepository.findById(id);
     }
 
-    @Transactional
-    public RoomDto updateRoom(final UUID id, final RoomDto roomDto) {
-        return roomRepository.findById(id)
-                             .map(existingRoom -> updateEntityAndConvertToDto(
-                                 roomDto,
-                                 existingRoom)).orElseThrow(() -> new ResourceNotFoundException(
-                ROOM_NOT_FOUND.getMessage(String.valueOf(id))));
-    }
-
-    @Transactional
-    public void deleteRoom(final UUID id) {
-        final Room room = roomRepository.findById(id)
-                                        .orElseThrow(() -> new ResourceNotFoundException(
-                                            ROOM_NOT_FOUND.getMessage(String.valueOf(id))));
+    @Override
+    public void delete(final Room room) {
         roomRepository.delete(room);
-    }
-
-    private RoomDto updateEntityAndConvertToDto(final RoomDto dto,
-                                                final Room entity) {
-        roomMapper.updateEntityFromDto(dto, entity);
-        return saveEntityAndConvertToDto(entity);
-    }
-
-    private RoomDto saveEntityAndConvertToDto(final Room entity) {
-        final Room savedEntity = roomRepository.save(entity);
-        return roomMapper.toDto(savedEntity);
     }
 }

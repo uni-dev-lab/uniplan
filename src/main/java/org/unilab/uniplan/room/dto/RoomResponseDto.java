cd5b74c8d15b58ffde706c9a1d645dd589bd58a0
PIPetkova19 <PIPetkova19@codingburgas.bg>
2026-07-08 09:49:30 +0300

    Add tests for room service

--- src/main/java/org/unilab/uniplan/room/dto/RoomResponseDto.java
+++ src/main/java/org/unilab/uniplan/room/dto/RoomResponseDto.java
@@ -1,7 +1,5 @@
 package org.unilab.uniplan.room.dto;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import java.util.UUID;
 
 public record RoomResponseDto(
--- src/test/java/org/unilab/uniplan/room/RoomServiceTest.java
+++ src/test/java/org/unilab/uniplan/room/RoomServiceTest.java
@@ -1,10 +1,6 @@
 package org.unilab.uniplan.room;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -17,10 +13,59 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.unilab.uniplan.exception.ResourceNotFoundException;
 
 @ExtendWith(MockitoExtension.class)
 class RoomServiceTest {
 
+    @Mock
+    private RoomRepository roomRepository;
+    @InjectMocks
+    private RoomService roomService;
+    private Room room;
+    private UUID id;
+
+    @BeforeEach
+    void setUp() {
+        id = UUID.randomUUID();
+        room = new Room();
+    }
+
+    @Test
+    void save_shouldSaveEntity() {
+        when(roomRepository.save(room)).thenReturn(room);
+
+        roomService.save(room);
+
+        verify(roomRepository).save(room);
+    }
+
+    @Test
+    void findAll_shouldReturnListOfEntities() {
+        List<Room> rooms = List.of(room);
+        when(roomRepository.findAll()).thenReturn(List.of(room));
+
+        List<Room> result = roomService.getAll();
+
+        verify(roomRepository).findAll();
+        assertEquals(result, rooms);
+    }
+
+    @Test
+    void findById_shouldReturnEntity_whenRoomExists() {
+        Optional<Room> expected = Optional.of(room);
+        when(roomRepository.findById(id)).thenReturn(expected);
+
+        Optional<Room> result = roomService.getById(id);
+
+        verify(roomRepository).findById(id);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void delete_shouldDeleteEntity_whenFacultyExists() {
+        roomService.delete(room);
+
+        verify(roomRepository).delete(room);
+    }
 
 }

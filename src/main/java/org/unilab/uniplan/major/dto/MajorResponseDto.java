beeb324dcfebbd827f252e99a6108878a91cb5cb
Djesika Vacheva <stu2401261031@uni-plovdiv.bg>
2026-07-16 14:19:55 +0300

    remove validations from MajorResponseDto

--- src/main/java/org/unilab/uniplan/major/dto/MajorResponseDto.java
+++ src/main/java/org/unilab/uniplan/major/dto/MajorResponseDto.java
@@ -1,15 +1,10 @@
 package org.unilab.uniplan.major.dto;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import java.util.UUID;
 
 public record MajorResponseDto(
     UUID id,
-    @NotNull
     UUID facultyId,
-    @NotNull
-    @Size(max = 200)
     String majorName
 ) {
 

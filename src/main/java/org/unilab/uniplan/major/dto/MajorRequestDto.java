5bc5973e2cf4fff49a7c8b66a0cbf47ae3a01dd2
Djesika Vacheva <stu2401261031@uni-plovdiv.bg>
2026-07-16 14:24:36 +0300

    add validation messages to MajorRequestDto

--- src/main/java/org/unilab/uniplan/major/dto/MajorRequestDto.java
+++ src/main/java/org/unilab/uniplan/major/dto/MajorRequestDto.java
@@ -1,14 +1,15 @@
 package org.unilab.uniplan.major.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.util.UUID;
 
 public record MajorRequestDto(
-    @NotNull
+    @NotNull(message = "Faculty id cannot be null")
     UUID facultyId,
-    @NotNull
-    @Size(max = 200)
+    @NotBlank(message = "Major name cannot be null")
+    @Size(max = 200, message = "Major name must be at most 200 characters")
     String majorName
 ) {
 
